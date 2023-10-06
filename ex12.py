d085bd3121aed0715d3e66f180cba581c16acfe3
SunFlow4me <dudosatv@mail.ru>
2023-10-06 17:32:49 +0500

    YTN CJJ,OTYBZ

---
+++ ex12.py
@@ -0,0 +1,6 @@
+age = input("твой возраст ")
+height = input("твой рост ")
+
+print(f"""
+возраст {age} лет
+рост {height} см""")
---
+++ ex5.py
@@ -0,0 +1,8 @@
+height = 188 # см
+weight = 80 # кг
+
+height_m = height / 100
+weight_t = weight / 1000
+
+print(f"рост в см = {height}, рост в метрах = {height_m}")
+print(f"вес в кг = {weight}, вес в тоннах = {weight_t}")
